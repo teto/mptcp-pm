e21aafb504dd1803d1b1a1f478bf36fde58cbb54
Matthieu Coudron <mattator@gmail.com>
2018-11-28 17:49:01 +0900

    fixing install

--- hs/shell-test.nix
+++ hs/shell-test.nix
@@ -13,7 +13,7 @@ in
 haskellPackages.shellFor {
   packages = p: with p; [ haskellPackages.netlink-pm ];
   withHoogle = true;
-  nativeBuildInputs = [ hie haskellPackages.stack ];
+  nativeBuildInputs = [ hie haskellPackages.stack haskellPackages.cabal-install ];
 
   # export HIE_HOOGLE_DATABASE=$NIX_GHC_DOCDIR as DOCDIR doesn't exist it won't work
   shellHook = ''
