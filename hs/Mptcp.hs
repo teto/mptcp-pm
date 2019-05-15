9874c6bf830483e63a322a3996ed143fbb135a64
Matthieu Coudron <mattator@gmail.com>
2019-05-15 22:39:39 +0900

    WIP

--- hs/Mptcp.hs
+++ hs/Mptcp.hs
@@ -1,3 +1,16 @@
+{-|
+Module      : MPTCP
+Description : Implementation of mptcp netlink path manager
+Maintainer  : matt
+Stability   : testing
+Portability : Linux
+
+-}
+module Mptcp
+where
+
+import Data.Word (Word8)
+
 tcpMetricsGenlName :: String
 tcpMetricsGenlName = "tcp_metrics"
 tcpMetricsGenlVer :: Word8
