e9f6e0861e7cb0010e6450417994e92bd086b8d9
Matthieu Coudron <mattator@gmail.com>
2019-07-17 18:24:10 +0900

    WIP

--- vm.nix
+++ vm.nix
@@ -4,43 +4,40 @@ let
   userNixpkgs = /home/teto/nixpkgs;
   customOverlay = /home/teto/home;
   vlans = [ 0 1 ];
-
 in
 {
   # imports = [
-  #   # /home/teto/dotfiles/nixpkgs/account-teto.nix
+  #   /home/teto/dotfiles/nixpkgs/account-teto.nix
   # ];
 
   # cmdline="root=/dev/sda1 earlycon=ttyS0 console=ttyS0 init=/nix/var/nix/profiles/system/init boot.debug=1 boot.consoleLogLevel=1 nokaslr tcp_probe.port=5201 tcp_probe.full=1";
   # # # x86_64 is a symlink towards x86
   # kernel="/home/teto/mptcp/build/arch/x86_64/boot/bzImage";
 
+  # services.mingetty.helpLine = "[9;0][14;0]";
+
   # Will add an eth
   virtualisation.vlans = vlans;
-  virtualisation.qemu.networkingOptions = lib.mkForce [ ];
+  # lib.mkForce 
+  virtualisation.qemu.networkingOptions = [ ];
 
-  # boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = true;
 
+  # slirpvde -s vde0.ctl -dhcp
+  # one can 
   environment.systemPackages = with pkgs; [
     neovim
+    dhcp # allows to run dhclient
     # mptcpnumerics  # from my overlay ? copy it here
   ];
 
-  # boot.initrd.supportedFilesystems
-  # /mnt-root//nix/var/nix/profiles/system/init
-  # cmdline="root=/dev/sda1 earlycon=ttyS0 console=ttyS0 init=/nix/var/nix/profiles/system/init boot.debug=1 raid=noautodetect nokaslr
-  # virtualisation.qemu.options =
-    # [
-    # "-kernel /home/teto/mptcp2/build/arch/x86_64/boot/bzImage"
-    # boot.consoleLogLevel=1
-    # ''-append "root=/dev/sda1 earlycon=ttyS0 console=ttyS0 init=/nix/var/nix/profiles/system/init boot.debug=1  nokaslr tcp_probe.port=5201 tcp_probe.full=1"''
-    # si j'utilise mon propre kernel g besoin de mon propre initramfs
-    # "-initrd "
-  # ];
-
-  # networking.hostName = "netlink";
-
   services.xserver.enable = false;
+  
+  networking = {
+    useDHCP = false;
+      localCommands = ''
+        echo "HELLO YOU";
+      '';
+  };
 
   # "nixos=https://github.com/nixos/nixpkgs-channels/archive/nixos-19.03.tar.gz"
   nix = {
@@ -50,14 +47,16 @@ in
     ++ pkgs.stdenv.lib.optional (builtins.pathExists userNixpkgs)  "nixpkgs=${builtins.toString userNixpkgs}";
   };
 
+  networking.mptcp.enable = true;
+
   # mountNix
-  nixos-shell.mounts.extraMounts = {
+  # nixos-shell.mounts.extraMounts = {
 
-    # "/mnt/examples" = ./.;
+  #   # "/mnt/examples" = ./.;
 
-    "/mnt/nixos-shell" = {
-      target = ./..;
-      cache = "none";
-    };
-  };
+  #   "/mnt/nixos-shell" = {
+  #     target = ./..;
+  #     cache = "none";
+  #   };
+  # };
 }
