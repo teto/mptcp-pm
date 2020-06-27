ef975095521a04fed3a38688d65f71f3e7acf3c4
Matthieu Coudron <mattator@gmail.com>
2020-06-27 23:22:56 +0200

    small cleanup to compile

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
+{ compiler }:
 let
 
-  compiler = pkgs.haskell.packages.ghc865;
+  # compiler = pkgs.haskell.packages.ghc883;
 
   cabal2_nixpkgs = import ./pinned_nixpkgs.nix;
   pkgs = cabal2_nixpkgs.pkgs;
--- pinned_nixpkgs.nix
+++ pinned_nixpkgs.nix
@@ -16,15 +16,12 @@ let
           #   };
           # };
 
-
-          # c2hsc = appendConfigureFlag hold.c2hsc "--with-gcc=${prev.gcc}/bin/gcc";
-          c2hsc = appendPatch hold.c2hsc (prev.fetchpatch {
-            url = "https://github.com/jwiegley/c2hsc/commit/490ecab202e0de7fc995eedf744ad3cb408b53cc.patch";
-            # sha256 = "12g462qmj8c7if797gnyvf8h0cddmm3xy0pjldw48w8f8sr4qsj0";
-            # sha256 = "sha256-QGpMskYOcUR4o/IC30etrTEAkdvevpOOi4chWbEw5Ik=";
-            sha256 = "sha256-B0OcZkVrLTBCCBJL+b4vfxSTBDfTT5lKYOie3Pe187A=";
-
-          });
+          # c2hsc = appendPatch hold.c2hsc (prev.fetchpatch {
+          #   url = "https://github.com/jwiegley/c2hsc/commit/490ecab202e0de7fc995eedf744ad3cb408b53cc.patch";
+          #   # sha256 = "12g462qmj8c7if797gnyvf8h0cddmm3xy0pjldw48w8f8sr4qsj0";
+          #   # sha256 = "sha256-QGpMskYOcUR4o/IC30etrTEAkdvevpOOi4chWbEw5Ik=";
+          #   sha256 = "sha256-B0OcZkVrLTBCCBJL+b4vfxSTBDfTT5lKYOie3Pe187A=";
+          # });
 
           # can be released on more recent nixplks
           # wide-word = doJailbreak (hold.wide-word);
@@ -33,6 +30,12 @@ let
           # for newer nixpkgs (March 2020)
           # base-compat = doJailbreak (hold.base-compat);
           # time-compat = doJailbreak (hold.time-compat);
+
+          # rebase needs time == 1.9.*
+          # time is a core package of ghc
+          # time = hold.time_1_10;
+          # time = null;
+          # rebase = doJailbreak (hold.rebase);
 
           netlink = (overrideSrc hold.netlink {
             # src = builtins.fetchGit {
