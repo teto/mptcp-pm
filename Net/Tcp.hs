63b09dd363261c937dde706d48192deefcd727f6
Matthieu Coudron <mcoudron@hotmail.com>
2021-02-22 15:03:15 +0100

    cleanup

--- Net/Tcp.hs
+++ Net/Tcp.hs
@@ -10,29 +10,11 @@ Portability : Linux
 module Net.Tcp (
     module Net.Tcp.Definitions
     , module Net.Tcp.Constants
-    -- , tcpFlagRef
-    -- , numberToTcpFlags
 ) where
 
 import Net.Tcp.Definitions
 import Net.Tcp.Constants
 import Net.Bitset
 
--- import Data.Bits ((.&.))
--- import Data.EnumSet
-
--- -- TODO these should be generated
--- tcpFlagRef :: TcpFlag -> Int
--- tcpFlagRef TcpFlagFin = 1
--- tcpFlagRef TcpFlagSyn = 2
--- tcpFlagRef TcpFlagAck = 8
-
 
 instance ToBitMask TcpFlag
-
--- numberToTcpFlags :: Int -> [TcpFlag]
--- numberToTcpFlags n = Prelude.filter  (\x -> combi x /= 0 ) list
---   where
---     combi x = (.&.) n (tcpFlagRef x)
---     list = [TcpFlagSyn, TcpFlagAck, TcpFlagFin ]
-
