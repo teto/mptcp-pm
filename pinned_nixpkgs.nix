7dd7a53be10f82ebaa90284538429474f531de8b
Matthieu Coudron <mcoudron@hotmail.com>
2021-05-07 21:18:55 +0200

    cleanup

--- pinned_nixpkgs.nix
+++ pinned_nixpkgs.nix
@@ -6,37 +6,9 @@ let
           ip = dontCheck hold.ip;
           bytebuild = dontCheck hold.bytebuild;
 
-          # ip = dontCheck overrideSrc hold.ip {
-          #   src = prev.fetchFromGitHub {
-          #     owner = "andrewthad";
-          #     repo = "haskell-ip";
-          #     rev = "1.7.2";
-          #     # sha256 = 
-          #     sha256 = "sha256-OL5KkVsoGAZskfwj8HAq2aBh60F2TW+WnAUfTjiekNc=";
-          #     # sha256 = "sha256-QGpMskYOcUR4o/IC30etrTEAkdvevpOOi4chWbEw5Ik=";
-          #   };
-          # };
-
-          # c2hsc = appendPatch hold.c2hsc (prev.fetchpatch {
-          #   url = "https://github.com/jwiegley/c2hsc/commit/490ecab202e0de7fc995eedf744ad3cb408b53cc.patch";
-          #   # sha256 = "12g462qmj8c7if797gnyvf8h0cddmm3xy0pjldw48w8f8sr4qsj0";
-          #   # sha256 = "sha256-QGpMskYOcUR4o/IC30etrTEAkdvevpOOi4chWbEw5Ik=";
-          #   sha256 = "sha256-B0OcZkVrLTBCCBJL+b4vfxSTBDfTT5lKYOie3Pe187A=";
-          # });
-
           # can be released on more recent nixplks
           # wide-word = doJailbreak (hold.wide-word);
           # quickcheck-classes = hold.quickcheck-classes_0_6_4_0;
-
-          # for newer nixpkgs (March 2020)
-          # base-compat = doJailbreak (hold.base-compat);
-          # time-compat = doJailbreak (hold.time-compat);
-
-          # rebase needs time == 1.9.*
-          # time is a core package of ghc
-          # time = hold.time_1_10;
-          # time = null;
-          # rebase = doJailbreak (hold.rebase);
 
           netlink = (overrideSrc hold.netlink {
             # src = builtins.fetchGit {
@@ -51,14 +23,6 @@ let
             };
           });
 
-          # TODO change source
-          # bitset = pkgs.haskell.lib.overrideSrc hold.bitset { src = pkgs.fetchFromGithub {
-          #     owner = "teto";
-          #     repository = "bitset";
-          #     rev = "upgrade";
-          #     sha256 = "0j0hrzr9b57ifwfhggpzm43zcf6wcsj8ffxv6rz7ni7ar1x96x2c";
-          #   };
-          # };
 
         };
       };
